8a2c6ab89e265bb237404cd0044d7fdd55da6c48
Piggey <pepcio3320@gmail.com>
2019-03-31 15:21:11 +0200

    make it better

--- secrets.py
+++ secrets.py
@@ -1,4 +1,5 @@
-consumer_key = 'Z20y4wzRhTu4jYUY1pGtB6WWM'
-consumer_secret = '5EBjrUW63VpFNzvAKsCr3hEFbplEaqYz3FQTPscG2AZqwtVStO'
-access_token = '1110105819478151168-lT665N1LQ4rtsAGfFCKtweetBvJ1gz'
-access_secret = 'gD5F3pF7OtxsZLb5envY46mzMK4rGO0QcVqcTAO1719Hr'
+from os import environ
+consumer_key = environ['Z20y4wzRhTu4jYUY1pGtB6WWM']
+consumer_secret = environ['5EBjrUW63VpFNzvAKsCr3hEFbplEaqYz3FQTPscG2AZqwtVStO']
+access_token = environ['1110105819478151168-lT665N1LQ4rtsAGfFCKtweetBvJ1gz']
+access_secret = environ['gD5F3pF7OtxsZLb5envY46mzMK4rGO0QcVqcTAO1719Hr']
