5c16c4851ff7c1c5f78cbc5d767fade8dbea0c89
Piggey <49120531+Piggey@users.noreply.github.com>
2019-07-09 17:42:51 +0200

    oh my god

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-import tweepy, os, random, time, requests, smtplib
+import tweepy, os, random, time, requests, smtplib, sys
 from bot_setup import api, mail, passw
 
-def selected_image():
+def select_image_from_url():
     img_url = random.choice(images)
     images.remove(img_url)
     print("Selected image " + img_url)
@@ -13,13 +13,20 @@ def selected_image():
                 image.write(chunk)
     return filename
 
+def select_image_from_file():
+    files = os.listdir("Memes/")
+    img = random.choice(files)
+    print("Selected file: " + img)
+    img_path = "Memes/" + img
+    return img_path
+
 def tweet_image():
     try:
-        api.update_with_media(selected_image())
-        os.remove("temp.jpg")
+        api.update_with_media(select_image_from_file())
+        #os.remove("temp.jpg")
         print("Image sent successfully!")
-    except Exception:
-        print(Exception)
+    except:
+        print("Failed to send the image to Twitter.")
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -35,22 +42,23 @@ def send_mail():
     print("Email sent!")
     server.quit()
 
-#loading the images from the images.txt file
-try:
-    images = open("images.txt",'r').read().split('\n')
-    print("Images loaded successfully.")
-    num_of_imgs = len(images)
-    print("Images loaded: " + str(num_of_imgs) + "\n")
-except Exception:
-    print(Exception)
+# #loading the images from the images.txt file
+# try:
+#     images = open("images.txt",'r').read().split('\n')
+#     print("Images loaded successfully.")
+#     num_of_imgs = len(images)
+#     print("Images loaded: " + str(num_of_imgs) + "\n")
+# except:
+#     print("Failed to load the images.")
 
+# #loop executing itself everyday at the time of the launch of the script
+# while(True):
+#     if(num_of_imgs == 0):
+#         send_mail()
 
-#loop executing itself everyday at the time of the launch of the script
-while(True):
-    if(num_of_imgs == 0):
-        send_mail()
+#     tweet_image()
+#     num_of_imgs = len(images)
+#     print("Images left: " + str(num_of_imgs))
+#     time.sleep(86400)
 
-    tweet_image()
-    num_of_imgs = len(images)
-    print("Images left: " + str(num_of_imgs))
-    time.sleep(86400)
+tweet_image()
